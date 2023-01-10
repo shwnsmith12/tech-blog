fa1d34348b289f53450f487e81f366fd3a8a17dd
Shawn <shwnsmith12@gmail.com>
2023-01-10 14:14:01 -0600

    added handlebars data

--- server.js
+++ server.js
@@ -2,44 +2,34 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
-// Importing Helper Function
 const helpers = require('./utils/helpers');
-
-// Importing Handlebars w/ custom helpers
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
-
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
+const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {maxAge: 25000},
+    cookie: { maxAge: 36000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize
     })
-  };
+};
 
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-  
-// Using Handlebars as Default Template
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-  
-// Turing on the Routes
 app.use(routes);
-  
-// Connection to the Database & Server
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening!'));
+    app.listen(PORT, () => console.log('Now listening!'));
 });
